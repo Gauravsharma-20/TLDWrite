0cd4057979c3c8abd1d26003cb1781a5c22987b2
Amit-Singh19 <amitsingh.tech19@gmail.com>
2021-10-31 16:07:05 +0530

    FIle uploader component Revamped

--- client/src/views/FileUploader/index.js
+++ client/src/views/FileUploader/index.js
@@ -1,53 +1,100 @@
-import {useState} from 'react';
-import { toast, ToastContainer} from 'react-toastify';
+import { useState } from 'react';
+import { ProgressBar } from 'react-bootstrap';
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+
 import FileDownload from 'js-file-download';
 
 import axios from 'axios';
 
+import "react-toastify/dist/ReactToastify.css";
 import './fileUploader.css';
 
-export const FileUploader = ({onSuccess}) => {
-    const [file, setFile] = useState([]);
+const FileUploader = ({onSuccess}) => {
 
-    const onInputChange = (e) => {
-        setFile(e.target.files[0])
+    const [file, setFile] = useState([]);
+    const [uploadPercentage, setUploadPercentage]=useState(0);
+
+    const successToast=()=>{
+        toast("File Uploaded Successfully",{
+            className:"fp11Toast",
+            draggable:true,
+            position:toast.POSITION.TOP_CENTER 
+         });
+    }
+    
+    const errorToast=()=>{
+        toast("Error In File Uploading",{
+            className:"fp11Toast",
+            draggable:true,
+            position:toast.POSITION.TOP_CENTER 
+         });
+    }
+    
+    const onInputChange = (event) => {
+        setFile(event.target.files[0])
     };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
         const data = new FormData();
         data.append('file', file);
 
-        const config = {
-            headers: {
-                'content-type':'multipart/form-data'
+        const options = {
+            onUploadProgress: (progressEvent) => {
+              const {loaded, total} = progressEvent;
+              let percent = Math.floor( (loaded * 100) / total )
+              console.log( `${loaded}kb of ${total}kb | ${percent}%` );
+      
+              if( percent < 100 ){
+                setUploadPercentage(percent);
+              }
             }
-        }
+          }
 
         try {
-            const response = await axios.post('//localhost:5000/speechtotext', data, config)
-            alert("Success")
+            const response = await axios.post('//localhost:5000/speechtotext', data, options)
+            // alert("Success");
+            successToast();
             FileDownload(response.data, 'report.txt');
         }
-        catch(e) {
-            alert(e);
+        catch(event) {
+            // alert(e);
+            errorToast();
         }
     };
 
     return (
+        <div className="fp11UploaderWrapper">
         <form method="post" action="#" id="#" onSubmit={onSubmit}>
+            <label className="fp11Label">Upload Your File </label>
             <div className="fp11FileUploader">
-                <div className="fp11formGroup fp01files">
-                    <label className="fp11Label">Upload Your File </label>
-                    <input type="file" onChange={onInputChange} className="fp11formControl"/>
+                <div className="fp11formGroup fp01files"> 
+                <ToastContainer 
+                draggable={false}
+                transition={Bounce}
+                autoClose={5000}
+                />
+                <i class="fp11Icon fas fa-cloud-upload-alt"></i>
+                <header>Drag and Drop Files</header>
+                    <span>OR</span>
+                <button>Browse</button>
+                <input 
+                type="file" 
+                onDrop={onInputChange}
+                onDragOver={(event)=>{
+                    event.preventDefault();
+                    event.stopPropagation();
+                }}
+                onChange={onInputChange} 
+                className="fp11formControl" 
+                hidden
+                /> 
                 </div>
-                <div className="fp11Button">
-                <button >Submit</button>
-                <ToastContainer />
-                </div>
-            </div>    
+            </div>     
         </form>
+        { uploadPercentage > 0 ? <ProgressBar now={uploadPercentage} active label={`${uploadPercentage}%`} />:null }
+        </div>
     )
 };
 
