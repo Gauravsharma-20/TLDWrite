551da54f058fef5327a55b35e32cc7c9ab1d8e29
Amit-Singh19 <amitsingh.tech19@gmail.com>
2021-10-31 16:06:09 +0530

    Button and Route Added

--- client/src/views/HomePage/index.js
+++ client/src/views/HomePage/index.js
@@ -1,23 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import './homePage.css';
 
-class HomePage extends React.Component{
-  render(){
-    return (
-      <div id="hp01Homepage">
-       <div className="hp01TopBar">
-         <div className="hp01In">
-           Sign In &nbsp; &nbsp; &nbsp;Sign Up
-         </div>
-        </div> 
-       <div className="hp01Summariser">
-         Meeting Summariser <br/><br/>
-         Enter
-       </div>
-      </div>
-   )
+const HomePage=()=>{
+  const history = useHistory();
+
+  const handleButtonClick=()=>{
+    history.push('/FileUploader');
   }
+
+  return (
+    <div id="hp01Homepage">
+      <div className="hp01TopBar">
+        <div className="hp01In">
+          Sign In &nbsp; &nbsp; &nbsp;Sign Up
+        </div>
+      </div> 
+      <div className="hp01Summariser">
+        Meeting Summariser<br/>
+        <button onClick={handleButtonClick}>Enter</button>
+      </div>
+    </div>
+   )
 }
 
 export default HomePage;
