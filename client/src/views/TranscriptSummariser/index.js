38ed27ddad47b52fa56879e0c300826b89f84eb2
Amit-Singh19 <amitsingh.tech19@gmail.com>
2021-12-13 06:34:30 +0530

    UI and transcript summariser component

---
+++ client/src/views/TranscriptSummariser/index.js
@@ -0,0 +1,7 @@
+import FileUploader from "../FileUploader";
+
+const TranscriptSummariser = () => {
+    return <FileUploader action="TextSummarisation"/>
+};
+
+export default TranscriptSummariser;
